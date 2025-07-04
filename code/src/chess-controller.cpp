8cb113762f6350db138f8cb52ca2e1bb59a37295
Arvilio <jelinsksk@gmail.com>
2025-07-04 23:27:53 +0300

    Shit man... I've nearly fucked my brains out here

--- code/src/chess-controller.cpp
+++ code/src/chess-controller.cpp
@@ -4,12 +4,17 @@
 
 int moveCount = 0;
 
-char fenState[] = "rnbqkb1r/pppppppp/8/8/8/8/PPPPPPPP/RNBQKB1R w KQkq - 0 1";
+char fenState[] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
 // tracking the physical state of the reed switches
 // 0 - filled; 1 - not filled
 int reedSwitchStates[64];
 int new_reedSwitchStates[64];
+
+// tracks the changed switch states. Updates in the compareboard function after each move
+int startMove;
+int endMove;
+
 
 // Used by the multiplexers to select the channels
 void selectChannel(int ch) {
@@ -23,36 +28,93 @@ void selectChannel(int ch) {
 void generateFENFromState(int* state) {
   String fen = "";
 
-  for (int rank = 0; rank < 8; rank++) {
-    int emptyCount = 0;
+  // for (int rank = 0; rank < 8; rank++) {
+  //   int emptyCount = 0;
 
-    for (int file = 0; file < 8; file++) {
-      int index = rank * 8 + file;
-      int square = state[index];
+  //   for (int file = 0; file < 8; file++) {
+  //     int index = rank * 8 + file;
+  //     int square = state[index];
 
-      if (square == 1) {
-        emptyCount++;
-      } else {
-        if (emptyCount > 0) {
-          fen += String(emptyCount);
-          emptyCount = 0;
+  //     if (square == 1) {
+  //       emptyCount++;
+  //     } else {
+  //       if (emptyCount > 0) {
+  //         fen += String(emptyCount);
+  //         emptyCount = 0;
+  //       }
+  //       fen += "P"; // Placeholder for a piece
+  //     }
+  //   }
+
+  //   if (emptyCount > 0) {
+  //     fen += String(emptyCount);
+  //   }
+
+  //   if (rank < 7) {
+  //     fen += "/";
+  //   }
+  // }
+
+  // take the old fen
+  int count = 0;
+  char startMovePiece;
+
+  // finding the starting piece
+  for (size_t i = 0; i < sizeof(fenState); i++) {
+    // range of numbers (val 1-8)
+    // before you move on make sure to check if you haven't jumped over. The necessary position.
+    for (int j = 0; j < 8; j++) {
+      if (fenState[i] == char(j)) {
+        if (count + j < startMove) {
+          count += j;
+        } else if (count + j > startMove) {
+          for (int k = count + j; j >= startMove; k--) {
+            Serial.print("Reducing from: ");
+            Serial.println(k);
+            if (k == startMove) {
+              Serial.print("Found: ");
+              Serial.println(k);
+              startMovePiece = fenState[i];
+            }
+          }
         }
-        fen += "P"; // Placeholder for a piece
-        // compare the previous state with the new state
-        
-        // figure out which piece has moved and assign it to the new position
-
       }
     }
 
-    if (emptyCount > 0) {
-      fen += String(emptyCount);
+    // piece id (val 1)
+    char piece = toLowerCase(fenState[i]);
+    
+    if ((piece == 'p' || piece == 'r' || piece == 'n' || 
+        piece == 'b' || piece == 'q' || piece == 'k') && count != startMove) {
+        count++;
+    } else if ((piece == 'p' || piece == 'r' || piece == 'n' || 
+                piece == 'b' || piece == 'q' || piece == 'k') && count == startMove) {
+        startMovePiece += fenState[i];  // Keep original case
+    }
+    // "/" ( skip to the next; i++)
+
+
+
+    if (count == 63) {
+      startMovePiece = fenState[i];
     }
 
-    if (rank < 7) {
-      fen += "/";
-    }
+
+
+    
   }
+
+  Serial.print("StartMovePiece Var: ");
+  Serial.println(startMovePiece);
+
+  Serial.print("startMove Var: ");
+  Serial.println(startMove);
+
+  // find the starting move pos. 
+  // Save the piece id.
+  // replace with an empty place
+  // find the ending move pos
+  // adjust the /..../ accordingly
 
   fen.toCharArray(fenState, sizeof(fenState));
 }
@@ -147,20 +209,23 @@ void compareBoard()
 {
   for (int i = 0; i < 64; i++) {
     if (reedSwitchStates[i] != new_reedSwitchStates[i]) {
-        // printf("Change at square %d: %d -> %d\n", i, reedSwitchStates[i], new_reedSwitchStates[i]);
-
-        Serial.print("Change at square ");
-        Serial.print(i);
-        Serial.print(": ");
-        Serial.print(reedSwitchStates[i]);
-        Serial.print(" -> ");
-        Serial.println(new_reedSwitchStates[i]);
+        if (reedSwitchStates[i] == 0 && new_reedSwitchStates[i] == 1)
+        {
+          startMove = i;
+        } else if (reedSwitchStates[i] == 1 && new_reedSwitchStates[i] == 0)
+        {
+          endMove = i;
+        }
     }
   }
 
   for (int i = 0; i < 64; i++) {
     reedSwitchStates[i] = new_reedSwitchStates[i];
   }
+
+  Serial.print(startMove);
+  Serial.print(": -> ");
+  Serial.println(endMove);
 }
 
 void moveBtnAction()
